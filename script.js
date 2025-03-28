3b9a6ba0414b7e34e87cb96c28536eefdce1ae2e
Szymon P <szymon_jozef@proton.me>
2025-03-28 16:15:47 +0100

    added script that counts days

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // SCRIPT HAHA
+
+
+function countDaysToEvent() {
+    const today = new Date();
+    // wstawić tu poprawną date
+    const eventDay = new Date(2025, 4, 29);
+    const diff = Math.ceil((eventDay - today) / (1000 * 60 * 60 * 24));
+
+    console.log(`There is ${diff} days left`);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    // można przypisać wartość tego do jakiegoś elementu
+    countDaysToEvent();
+});
